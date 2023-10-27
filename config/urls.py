0fd59d080da19f4ecda45a25b359af0a2a47a713
“django402” <djangoproject02@gmail.com>
2023-10-27 21:30:02 +0330

    not urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,16 +21,9 @@ from django.conf.urls.static import static
 
 
 
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("root.urls")),
-    #path("accounts/",include("accounts.urls")),
-    #path("accounts/",include("django.contrib.auth.urls")),
-
-    
 ]
 
 
